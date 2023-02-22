562edb389bd445c7d95673ced243b76129dd00cb
Ernesto <eechechele@gmail.com>
2023-02-22 11:38:40 +0100

    Prueba subir el script
    Ayer no quedó en git

--- script2.js
+++ script2.js
@@ -1,6 +1,24 @@
 //primer paso: seleccionar el elemento del DOM y guardarlos en la variable
 const scoreField = document.querySelector('.score')
 const checkButton = document.querySelector('.check')
+const againButton = document.querySelector('.again')
+const guessfield  = document.querySelector('.guess')
+const highscoreField = document.querySelector('.highscore')
+const messageField = document.querySelector('.message')
+const numberField = document.querySelector('.number')
+
+//segundo paso: crear la variable que va a guardar el valor del score
+
+console.log(
+  scoreField,
+  checkButton,
+  againButton,
+  guessfield,
+  highscoreField,
+  messageField,
+  numberField
+  )
+
 scoreField.textContent = 10
 
 checkButton.addEventListener('click', alerta)
